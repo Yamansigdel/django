b0e3ea13be416c6c1f8b4f525be294f20e0833bb
Yaman Sigdel <Yamansigdel999@gmail.com>
2023-11-19 09:18:44 +0545

    django

--- myproject/views.py
+++ myproject/views.py
@@ -2,17 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def homePage(request):
-    data={
-        'title':'Home Page',
-        'bdata':'Welcome to my project',
-        'clist':['PHP','JAVA','Python'],
-        'numbers':[10,20,30,40,50],
-        'student_details':[
-            {'name':'pradeep','phone':9840031339},
-            {'name':'testing','phone':9840031339},
-        ]
-    }
-    return render(request,"index.html",data)
+    # data={
+    #     'title':'Home Page',
+    #     'bdata':'Welcome to my project',
+    #     'clist':['PHP','JAVA','Python'],
+    #     'numbers':[10,20,30,40,50],
+    #     'student_details':[
+    #         {'name':'pradeep','phone':9840031339},
+    #         {'name':'testing','phone':9840031339},
+    #     ]
+    # }
+    return render(request,"index.html")
 
 def aboutUs(request):
     return HttpResponse("Welcome to myproject")
