9030a1b42ec4a148715da681efd38cc61a4e96c7
Yaman Sigdel <Yamansigdel999@gmail.com>
2023-12-03 20:04:38 +0545

    django

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myproject import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.homePage),
@@ -28,3 +30,6 @@ urlpatterns = [
     path('calculator/', views.calculator,name="calculator"),
     path('evenodd/', views.evenodd),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
